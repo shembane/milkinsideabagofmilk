8abf4fcf5fe208c2d1edc6f7a381cf8df18c2a30
ShemBane <shembane@users.noreply.github.com>
2022-08-26 14:15:09 +0300

    No

--- kilyourself.py
+++
@@ -1,3 +0,0 @@
-x = "milk inside of a bag inside of a bag of milk"
-y = " inside of a bag of milk "
-print(x + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y)
