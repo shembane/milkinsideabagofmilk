7499822c992e775da903f7a1b241ca3af902e012
Guy Levi <leviguy0904@gmail.com>
2022-08-26 14:08:57 +0300

    why.

---
+++ kilyourself.py
@@ -0,0 +1,3 @@
+x = "milk inside of a bag inside of a bag of milk"
+y = " inside of a bag of milk "
+print(x + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y + y)
