1671099a0d0e0e0fc2127374eaa8170a021dec4a
Guy Levi <leviguy0904@gmail.com>
2022-11-18 11:00:15 +0200

    Update kdowkfoekfekfekfefeofeof.py

--- God knows what this is/kdowkfoekfekfekfefeofeof.py
+++ God knows what this is/kdowkfoekfekfekfefeofeof.py
@@ -1,5 +1,9 @@
-from random import randint
+while True: 
+    from random import randint
+    from time import sleep
 
-for i in range(randint(1,10)):
-    print("eyal")
-    print("regev")
+    for i in range(randint(1,10)):
+        print("eyal")
+        print("regev")
+       
+    sleep(randint(1,2))
