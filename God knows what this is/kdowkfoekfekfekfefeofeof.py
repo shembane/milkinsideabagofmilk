1d6a0d295fcf1b3af62431aa79889e5175d147e0
AlonFranck <alonf64@gmail.com>
2022-11-19 13:14:03 +0200

    Update kdowkfoekfekfekfefeofeof.py

--- God knows what this is/kdowkfoekfekfekfefeofeof.py
+++ God knows what this is/kdowkfoekfekfekfefeofeof.py
@@ -2,8 +2,12 @@ while True:
     from random import randint
     from time import sleep
 
+    text_color = ""
+
+
     for i in range(randint(1,10)):
+        
         print("eyal")
         print("regev")
-       
+    print("\n+--------------------------------------------+\n")
     sleep(randint(1,2))
